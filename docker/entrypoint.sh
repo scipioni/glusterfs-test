df674bb38e8241f77a0666439eab42cf100b35d9
Stefano Scipioni <scipio.it@gmail.com>
2022-08-20 09:19:38 +0200

    2 newtorks connected

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,15 +1,29 @@
 
 #!/bin/bash
-#
-# This script is designed to be run inside the container
-#
 
-# fail hard and fast even on pipelines
-#set -meo pipefail
 
-service rpcbind start
+#$GW=$1
+
 
 # Disable rdma
 sed -i.save -e "s#,rdma##" /etc/glusterfs/glusterd.vol
 
-exec glusterd --log-file=- --no-daemon $@
+
+if [ -n "$GW" ]; then
+    echo "settings gateway to $GW"
+    ip route del default
+    ip route add default via $GW
+fi 
+
+if [ -n "$ROUTE" ]; then
+    echo "add route $ROUTE"
+    #iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE 
+    #iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
+    iptables -A FORWARD -i eth1 -j ACCEPT
+    iptables -A FORWARD -i eth0 -j ACCEPT
+    ip route add $ROUTE
+    exec tail -f /dev/null
+fi
+
+service rpcbind start
+exec glusterd --log-file=- --no-daemon
